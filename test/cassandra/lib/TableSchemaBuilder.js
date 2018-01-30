d80ff95b4c226d1e2ce6adefe37173307f9bade6
Alexei Torunov <merisho@ukr.net>
2018-01-30 14:14:18 +0200

    Table options configuration through JSON schema

--- test/cassandra/lib/TableSchemaBuilder.js
+++ test/cassandra/lib/TableSchemaBuilder.js
@@ -79,4 +79,53 @@ describe('Table Schema Builder', () => {
         const query = builder.build();
         assert.equal(query, 'CREATE TABLE my_table(col1 int,col2 int,col3 int,PRIMARY KEY((col1),col2,col3)) WITH CLUSTERING ORDER BY(col2 ASC,col3 DESC)');
     });
+
+    it('Should build query for table creation with table options', () => {
+        const tableSchema = {
+            col1: 'int',
+            __primaryKey__: [ 'col1' ],
+            __options__: {
+                bloom_filter_fp_chance: 0.1,
+                comment: '',
+                compaction: {
+                    'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy',
+                    'max_threshold': 32,
+                    'min_threshold': 4
+                }
+            }
+        };
+
+        const builder = new TableSchemaBuilder().createTable('my_table').fromJSONSchema(tableSchema);
+
+        let expectedQuery = `CREATE TABLE my_table(col1 int,PRIMARY KEY((col1)))`;
+        expectedQuery = `${expectedQuery} WITH bloom_filter_fp_chance = 0.1 AND comment = ''`;
+        expectedQuery = `${expectedQuery} AND compaction = {'class':'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy','max_threshold':32,'min_threshold':4}`;
+
+        const query = builder.build();
+        assert.equal(query, expectedQuery);
+    });
+
+    it('Should build query for table creation with table options AND ordering', () => {
+        const tableSchema = {
+            col1: 'int',
+            col2: 'int',
+            __primaryKey__: [ 'col1' ],
+            __clusteredKey__: [ 'col2' ],
+            __options__: {
+                bloom_filter_fp_chance: 0.1
+            },
+            __order__: {
+                col2: 'DESC'
+            }
+        };
+
+        const builder = new TableSchemaBuilder().createTable('my_table').fromJSONSchema(tableSchema);
+
+        let expectedQuery = `CREATE TABLE my_table(col1 int,col2 int,PRIMARY KEY((col1),col2))`;
+        expectedQuery = `${expectedQuery} WITH CLUSTERING ORDER BY(col2 DESC)`;
+        expectedQuery = `${expectedQuery} AND bloom_filter_fp_chance = 0.1`;
+
+        const query = builder.build();
+        assert.equal(query, expectedQuery);
+    });
 });
