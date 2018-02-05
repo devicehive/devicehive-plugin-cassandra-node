6b00dca2bde8c5c5a2222927bd20e7dab905b3e5
Alexei Torunov <merisho@ukr.net>
2018-02-05 15:16:44 +0200

    Add SUBSCRIPTION_GROUP for plugin, remove test files

--- create-schemas-test.js
+++
@@ -1,5 +0,0 @@
-const cassandraConfig = require('./plugin/config').cassandra;
-
-cassandraConfig.CONNECTION.KEYSPACE = 'testks';
-
-require('./plugin/createCassandraSchemas');
--- plugin-test.js
+++
@@ -1,9 +0,0 @@
-const cassandraConfig = require('./plugin/config').cassandra;
-
-cassandraConfig.CONNECTION.KEYSPACE = 'testks';
-
-const CassandraPlugin = require('./plugin/CassandraPluginService');
-
-new CassandraPlugin().initCassandra().then(cass => {
-    console.log('Cassandra initialized');
-}).catch(console.error);
