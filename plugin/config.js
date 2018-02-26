0a0de763defd2d71ca86ec4f046fb05f7b204830
Alexei Torunov <merisho@ukr.net>
2018-02-26 18:39:59 +0200

    Set ignoreCase flag as true in json configurator, update npm modules

--- plugin/config.js
+++ plugin/config.js
@@ -3,7 +3,8 @@ const configurator = require('json-evn-configurator');
 const Utils = require('./Utils');
 const cassandraTables = require('../cassandraSchemas/cassandra-tables');
 
-const cassandraConf = configurator(require('./cassandra-config'), 'cassandra');
+const IGNORE_CONFIG_CASE = true;
+const cassandraConf = configurator(require('./cassandra-config'), 'cassandra', IGNORE_CONFIG_CASE);
 
 cassandraConf.custom.commandUpdatesStoring = Utils.isNotEmpty(cassandraTables.commandUpdatesTables);
 
